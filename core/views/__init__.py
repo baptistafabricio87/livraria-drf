6dc87ef33b0fb1c4eda9c1e6e56337663d8dea12
Fabricio Castro <baptistafabricio87@gmail.com>
2023-07-03 22:43:48 -0300

    restructuring of project directories

---
+++ core/views/__init__.py
@@ -0,0 +1,5 @@
+from core.views.categoriaCBV import CategoriaView
+from core.views.categoriaAPIView import CategoriaDetail, CategoriaList
+from core.views.categoriaGenericAPIView import CategoriaListGenericAPIView, CategoriaDetailGenericAPIView
+from core.views.categoria import CategoriaViewSet
+
---
+++ core/views/categoriaGenericAPIView.py
@@ -0,0 +1,17 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from core.models import Categoria
+from core.serializers import CategoriaSerializer
+
+
+# Generics API View
+class CategoriaListGenericAPIView(ListCreateAPIView):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
+class CategoriaDetailGenericAPIView(RetrieveUpdateDestroyAPIView):
+    lookup_field = 'id'
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
